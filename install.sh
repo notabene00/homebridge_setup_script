53202d722e792dfb2c4207033f39b1c37178dff4
Елизаров Роман Русланович <fantomnotabene@gmail.com>
2018-05-13 23:59:38 +0300

    update ui plugin version, refactoring

--- install.sh
+++ install.sh
@@ -2,11 +2,9 @@
 
 curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
 sudo apt install -y nodejs libavahi-compat-libdnssd-dev
-sudo npm i -g --unsafe-perm mdns
-sudo npm i -g --unsafe-perm homebridge
 mkdir ~/.homebridge && cp config.json ~/.homebridge
 
-sudo npm install -g --unsafe-perm homebridge-config-ui-x@3.6.1
+sudo npm i -g --unsafe-perm mdns homebridge homebridge-config-ui-x@3.6.3-3
 
 sudo cp homebridge.service /etc/systemd/system/
 sudo systemctl daemon-reload
