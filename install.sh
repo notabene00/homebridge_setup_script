a5632edbda600a872bde9b1ba0c5c7bdad1f767c
Елизаров Роман Русланович <fantomnotabene@gmail.com>
2018-05-16 14:50:39 +0300

    Update install.sh
    Все-таки убрал версию ui. Неплохо бы временами следить за конфигом для этого плагина.

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
 sudo apt install -y nodejs libavahi-compat-libdnssd-dev
 mkdir ~/.homebridge && cp config.json ~/.homebridge
 
-sudo npm i -g --unsafe-perm mdns homebridge homebridge-config-ui-x@3.6.3-3
+sudo npm i -g --unsafe-perm mdns homebridge homebridge-config-ui-x
 
 sudo cp homebridge.service /etc/systemd/system/
 sudo systemctl daemon-reload
