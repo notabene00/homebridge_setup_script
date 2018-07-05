48b0265e43fe6751c01c58f71f4a58d4548eadea
Елизаров Роман Русланович <fantomnotabene@gmail.com>
2018-07-05 18:44:03 +0300

    Node.js 10

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 sudo apt install -y nodejs libavahi-compat-libdnssd-dev
 mkdir ~/.homebridge && cp config.json ~/.homebridge
 
-sudo npm i -g --unsafe-perm mdns homebridge homebridge-config-ui-x
+sudo npm i -g --unsafe-perm homebridge homebridge-config-ui-x
 
 sudo cp homebridge.service /etc/systemd/system/
 sudo systemctl daemon-reload
