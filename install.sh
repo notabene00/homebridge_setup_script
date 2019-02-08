09bd200e41828f30babf58af5d9758188c0edcbb
Елизаров Роман Русланович <fantomnotabene@gmail.com>
2019-02-08 13:57:51 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_11.x | sudo -E bash -
 sudo apt-get install -y nodejs libavahi-compat-libdnssd-dev
 mkdir ~/.homebridge && cp config.json ~/.homebridge
 
