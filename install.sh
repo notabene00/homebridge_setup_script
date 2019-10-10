984770b4b6c65173ae0e46df0624a64fd66e53c2
fantomnotabene <fantomnotabene@gmail.com>
2019-10-10 14:12:42 +0300

    Standalone homebridge-config-ui-x support

--- install.sh
+++ install.sh
@@ -6,7 +6,44 @@ mkdir ~/.homebridge && cp config.json ~/.homebridge
 
 sudo npm i -g --unsafe-perm homebridge homebridge-config-ui-x
 
-sudo cp homebridge.service /etc/systemd/system/
+cat > /etc/systemd/system/homebridge.service << EOL
+[Unit]
+Description=Homebridge Config UI X
+After=syslog.target network-online.target
+
+[Service]
+Type=simple
+User=$USER
+ExecStart=$(which homebridge-config-ui-x) -I -U /$USER/.homebridge
+Restart=on-failure
+RestartSec=3
+KillMode=process
+CapabilityBoundingSet=CAP_IPC_LOCK CAP_NET_ADMIN CAP_NET_BIND_SERVICE CAP_NET_RAW CAP_SETGID CAP_SETUID CAP_SYS_CHROOT CAP_CHOWN CAP_FOWNER CAP_DAC_OVERRIDE CAP_AUDIT_WRITE CAP_SYS_ADMIN
+AmbientCapabilities=CAP_NET_RAW
+
+[Install]
+WantedBy=multi-user.target
+EOL
+
+cat > /etc/systemd/system/homebridge-config-ui-x.service << EOL
+[Unit]
+Description=Node.js HomeKit Server 
+After=syslog.target network-online.target
+
+[Service]
+Type=simple
+User=$USER
+ExecStart=$(which homebridge) -I -U /$USER/.homebridge
+Restart=on-failure
+RestartSec=3
+KillMode=process
+
+[Install]
+WantedBy=multi-user.target
+EOL
+
 sudo systemctl daemon-reload
 sudo systemctl enable homebridge
 sudo systemctl start homebridge
+sudo systemctl enable homebridge-config-ui-x
+sudo systemctl start homebridge-config-ui-x
